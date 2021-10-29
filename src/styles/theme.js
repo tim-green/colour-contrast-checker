f8159336f1d8dc9e7d5c55a4fa072da54f225870
Tim <github@timgreen.ws>
2021-10-29 12:58:45 +1100

    added colors including hover states

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -9,6 +9,24 @@ const variables = css`
   :root {
     --background: ${defaultColors.background};
     --foreground: ${defaultColors.foreground};
+
+    --black: #101010;
+    --deep-black: #0A0909;
+    --darkest-gray: #3F3F3F;
+    --dark-gray: #292929;
+    --gray: #A6A6A6;
+    --white: #ffffff;
+    --white-hover: rgba(255, 255, 255, .2);
+    --red: #C30000;
+    --light-red: #E15346;
+    --light-red-hover: rgba(225, 83, 70, .2);
+    --greeny: #01D277;
+    --greeny-hover: rgba(1, 210, 119, .2);
+    --green: #5BB015;
+    --green-hover: rgba(91, 176, 21, .2);
+    --cyan: #2afeb7;
+    --cyan-hover: rgba(42, 254, 183, .7);
+
     --font-sans: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans",
       "Droid Sans", "Helvetica Neue", sans-serif;
     --gradient-start: rgba(17, 153, 142, 1);
