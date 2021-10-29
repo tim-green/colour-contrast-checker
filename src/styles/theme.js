7fdad03cb0bed824ac0c6347d70b574db69aa597
Tim <github@timgreen.ws>
2021-10-29 12:48:13 +1100

    added default colours for checker

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,1 +1,7 @@
 import { css } from "styled-components";
+
+export const defaultColors = {
+  background: "#101010", // rgb(16, 16, 16) or hsl(	0, 0%, 6%)
+  foreground: "#01D277", // rgb(1, 210, 119 ) or hsl(154°, 99%, 41%)
+};
+
