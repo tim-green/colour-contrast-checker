ecdc6fff01003066d29fd1c6abd3d8cea4b4bdc6
Tim <github@timgreen.ws>
2021-11-01 14:40:58 +1100

    added function for outline button

--- src/components/atoms/OutlineButton.jsx
+++ src/components/atoms/OutlineButton.jsx
@@ -1,1 +1,43 @@
 import styled from "styled-components";
+function OutlineButton({ anchor, children, color, hoverColor, hrefLink, icon, onClickEvent }) {
+  // Additional props if we cast the button into an anchor tag
+  const anchorProps = anchor
+    ? {
+        as: "a",
+        href: hrefLink,
+        target: "_blank",
+        rel: "noreferrer",
+      }
+    : {};
+
+  return (
+    <Button
+      {...anchorProps}
+      anchor={anchor}
+      foreground={color}
+      hoverColor={hoverColor}
+      hasIcon={icon !== undefined}
+      onClick={onClickEvent}
+    >
+      {icon}
+      <span>{children}</span>
+    </Button>
+  );
+}
+
+OutlineButton.propTypes = {
+  anchor: PropTypes.bool,
+  children: PropTypes.string,
+  color: PropTypes.string,
+  hoverColor: PropTypes.string,
+  hrefLink: PropTypes.string,
+  icon: PropTypes.node,
+  onClickEvent: PropTypes.func,
+};
+
+OutlineButton.defaultProps = {
+  anchor: false,
+  color: "var(--white)",
+};
+
+export default OutlineButton;
