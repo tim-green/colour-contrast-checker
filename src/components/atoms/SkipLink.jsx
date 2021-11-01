c665ef9cd6e88d74fb7fe6c0eb0327c249de476a
Tim <github@timgreen.ws>
2021-11-01 15:16:45 +1100

    added styles for skip links

--- src/components/atoms/SkipLink.jsx
+++ src/components/atoms/SkipLink.jsx
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+
+const SkipLink = styled.a`
+  padding: 1.2rem;
+  position: absolute;
+  top: -6rem;
+  background-color: var(--greeny);
+  border-radius: 0.5rem;
+  color: var(--black);
+  font-size: 1.6rem;
+  font-weight: 600;
+  z-index: 999;
+  transition: transform .15s ease;
+
+  &:focus, &:active {
+    transform: translateY(120%);
+    outline: none;
+  }
+
+  &:hover{
+    text-decoration: underline 2px var(--forest);
+    color: var(--deep-black);
+  }
+`;
+
+export default SkipLink;
