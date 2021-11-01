25323ec548beb12f05d7f5bc27e5ceb7d301fa2c
Tim <github@timgreen.ws>
2021-11-01 14:41:29 +1100

    added function for rating contrast

--- src/components/atoms/Rating.jsx
+++ src/components/atoms/Rating.jsx
@@ -1,3 +1,17 @@
 import { memo } from "react";
 import styled from "styled-components";
 import { bp } from "../../styles/breakpoints";
+function Rating({ pass }) {
+  return (
+    <Container pass={pass}>
+      {pass ? <MdCheck size={28} /> : <MdClose size={28} />}
+      <Text>{pass ? "Pass" : "Fail"}</Text>
+    </Container>
+  );
+}
+
+Rating.propTypes = {
+  pass: PropTypes.bool.isRequired,
+};
+
+export default memo(Rating);
