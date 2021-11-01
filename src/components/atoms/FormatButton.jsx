e4631968657312010fb580b1762dd77d44d7a2c1
Tim <github@timgreen.ws>
2021-11-01 14:40:00 +1100

    added function for format button

--- src/components/atoms/FormatButton.jsx
+++ src/components/atoms/FormatButton.jsx
@@ -1,1 +1,19 @@
 import styled from "styled-components";
+
+function FormatButton({ target }) {
+  const { handleChangeFormat } = useContext(ColorContext);
+
+  return (
+    <Tooltip content={<p>Change color format</p>} offset={[-40, 14]} trigger="mouseenter focusin">
+      <Button onClick={() => handleChangeFormat(target)} aria-label="Change color format">
+        <MdColorLens size={24} />
+      </Button>
+    </Tooltip>
+  );
+}
+
+FormatButton.propTypes = {
+  target: PropTypes.oneOf(["background", "foreground"]).isRequired,
+};
+
+export default FormatButton;
