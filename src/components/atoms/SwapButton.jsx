aa2c93342a6a6cbaf310eca8d38c0fad2537a375
Tim <github@timgreen.ws>
2021-11-01 14:43:39 +1100

    added function for swap button

--- src/components/atoms/SwapButton.jsx
+++ src/components/atoms/SwapButton.jsx
@@ -1,2 +1,20 @@
 import { useContext } from "react";
 import styled from "styled-components";
+export default function SwapButton() {
+  const { handleSwapColors } = useContext(ColorContext);
+
+  return (
+    <Tooltip
+      content={<p>Swap colors</p>}
+      trigger="mouseenter focusin"
+      placement="right"
+      offset={[0, 15]}
+      touch={false}
+      wrapperStyles={tooltipWrapperStyles}
+    >
+      <Button aria-label="Swap colors">
+        <MdSwapVert size={28} onClick={handleSwapColors} />
+      </Button>
+    </Tooltip>
+  );
+}
