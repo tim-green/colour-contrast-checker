4a103d232fdf8f1099d5b2b34b432d470fd1a56b
Tim <github@timgreen.ws>
2021-11-01 15:08:54 +1100

    added styles for ratings

--- src/components/atoms/Rating.jsx
+++ src/components/atoms/Rating.jsx
@@ -3,6 +3,27 @@ import styled from "styled-components";
 import { MdCheck, MdClose } from "react-icons/md";
 import PropTypes from "prop-types";
 import { bp } from "../../styles/breakpoints";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => (props.pass ? "var(--green)" : "var(--light-red)")};
+  color: ${(props) => (props.pass ? "var(--deep-black)" : "var(--deep-black)")};
+  border-radius: .4rem;
+  padding: 0.3rem 0.7rem;
+`;
+
+const Text = styled.p`
+  margin-left: 0.3rem;
+  font-size: 2.4rem;
+  font-weight: 600;
+  text-align: center;
+
+  @media (max-width: ${bp.md}) {
+    font-size: 2.2rem;
+  }
+`;
+
 function Rating({ pass }) {
   return (
     <Container pass={pass}>
