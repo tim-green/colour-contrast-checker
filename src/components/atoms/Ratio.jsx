9cd4add624b67282ecf86c85561db7796f05cd7f
Tim <github@timgreen.ws>
2021-11-01 15:09:09 +1100

    added styles for ratio

--- src/components/atoms/Ratio.jsx
+++ src/components/atoms/Ratio.jsx
@@ -2,6 +2,36 @@ import { useContext } from "react";
 import styled from "styled-components";
 import ColorContext from "../../context/ColorContext";
 import { bp } from "../../styles/breakpoints";
+
+const Container = styled.div`
+  width: 9.5rem;
+  position: relative;
+
+  @media (max-width: ${bp.md}) {
+    width: 9rem;
+  }
+`;
+
+const Value = styled.p`
+  display: inline-block;
+  color: var(--white);
+  font-size: 3.2rem;
+  font-weight: 600;
+
+  @media (max-width: ${bp.md}) {
+    font-size: 3rem;
+  }
+`;
+
+const OutOf = styled.p`
+  position: absolute;
+  bottom: 0.6rem;
+  right: 0;
+  display: inline-block;
+  color: var(--gray);
+  font-size: 1.4rem;
+`;
+
 export default function Ratio() {
   let { contrast } = useContext(ColorContext);
   contrast = (Math.round(contrast * 10) / 10).toFixed(1); // Round to 1 decimal place
