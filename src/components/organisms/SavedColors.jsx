40b7fe1ab531c78b1f42929716cca7d0ba7f3578
Tim <github@timgreen.ws>
2021-11-01 15:18:58 +1100

    added styles for saved colours

--- src/components/organisms/SavedColors.jsx
+++ src/components/organisms/SavedColors.jsx
@@ -2,6 +2,24 @@ import styled from "styled-components";
 import SaveButtonGroup from "../molecules/SaveButtonGroup";
 import ColorSwatch from "../molecules/ColorSwatch";
 import { bp } from "../../styles/breakpoints";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 36rem;
+  padding-top: 1.9rem;
+
+  @media (min-width: 900px) and (max-width: ${bp.lg}) {
+    padding-top: 1;
+    max-width: 38rem;
+  }
+
+  @media (min-width: ${bp.md}) and (max-width: 900px) {
+    padding-top: 0;
+    padding-left: 2rem;
+    max-width: 32rem;
+  }
+`;
+
 export default function SavedColors() {
   return (
     <Container id="saved-colors">
