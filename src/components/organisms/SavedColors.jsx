c742944fa8c7ded608fbd902d3f1981210338ddb
Tim <github@timgreen.ws>
2021-11-01 14:48:02 +1100

    added function for saved colors

--- src/components/organisms/SavedColors.jsx
+++ src/components/organisms/SavedColors.jsx
@@ -1,2 +1,11 @@
 import styled from "styled-components";
 import { bp } from "../../styles/breakpoints";
+export default function SavedColors() {
+  return (
+    <Container id="saved-colors">
+      <h2>Saved Colors</h2>
+      <SaveButtonGroup />
+      <ColorSwatch />
+    </Container>
+  );
+}
