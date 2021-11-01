75c7638eada97614fea81c391b7c0250e5a585e9
Tim <github@timgreen.ws>
2021-11-01 14:47:29 +1100

    added function for color inputs

--- src/components/organisms/ColorInputs.jsx
+++ src/components/organisms/ColorInputs.jsx
@@ -1,2 +1,18 @@
 import styled from "styled-components";
 import { bp } from "../../styles/breakpoints";
+
+export default function ColorInputs() {
+  return (
+    <Container>
+      <ColorInputBlock>
+        <InputLabel title="Background" labelFor="background" tooltipText="Accepts HEX, RGB & HSL" />
+        <ColorInputField target="background" />
+      </ColorInputBlock>
+      <SwapButton />
+      <ColorInputBlock>
+        <InputLabel title="Foreground" labelFor="foreground" tooltipText="Accepts HEX, RGB & HSL" />
+        <ColorInputField target="foreground" />
+      </ColorInputBlock>
+    </Container>
+  );
+}
