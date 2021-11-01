ea6b04ea405179ce245df33e9235181b0abadfd0
Tim <github@timgreen.ws>
2021-11-01 14:41:47 +1100

    added function for ratio

--- src/components/atoms/Ratio.jsx
+++ src/components/atoms/Ratio.jsx
@@ -1,3 +1,14 @@
 import { useContext } from "react";
 import styled from "styled-components";
 import { bp } from "../../styles/breakpoints";
+export default function Ratio() {
+  let { contrast } = useContext(ColorContext);
+  contrast = (Math.round(contrast * 10) / 10).toFixed(1); // Round to 1 decimal place
+
+  return (
+    <Container>
+      <Value>{contrast}</Value>
+      <OutOf>/21</OutOf>
+    </Container>
+  );
+}
