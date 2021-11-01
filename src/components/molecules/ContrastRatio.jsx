276413bf1f67e7962d9ce3d3d6f390b1c3b0ddc3
Tim <github@timgreen.ws>
2021-11-01 14:44:50 +1100

    added function for contrast ratio

--- src/components/molecules/ContrastRatio.jsx
+++ src/components/molecules/ContrastRatio.jsx
@@ -1,2 +1,11 @@
 import styled from "styled-components";
 import { bp } from "../../styles/breakpoints";
+
+export default function ContrastRatio() {
+  return (
+    <Container>
+      <Label>Contrast Ratio</Label>
+      <Ratio />
+    </Container>
+  );
+}
