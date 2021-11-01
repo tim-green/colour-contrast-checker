f9e8846ccb0948d42c3f3289033a1d414cfddcdd
Tim <github@timgreen.ws>
2021-11-01 14:46:12 +1100

    added function for input label

--- src/components/molecules/InputLabel.jsx
+++ src/components/molecules/InputLabel.jsx
@@ -1,1 +1,26 @@
 import styled from "styled-components";
+function InputLabel({ labelFor, title, tooltipText }) {
+  return (
+    <Container>
+      <Label htmlFor={labelFor}>{title}</Label>
+      {tooltipText !== undefined ? (
+        <Tooltip
+          placement={window.innerWidth >= 330 ? "right" : "top"}
+          content={<p>{tooltipText}</p>}
+          wrapperStyles={tooltipWrapperStyles}
+          trigger="mouseenter focusin"
+        >
+          <InfoIcon size={18} tabIndex="0" />
+        </Tooltip>
+      ) : null}
+    </Container>
+  );
+}
+
+InputLabel.propTypes = {
+  labelFor: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  tooltipText: PropTypes.string,
+};
+
+export default InputLabel;
