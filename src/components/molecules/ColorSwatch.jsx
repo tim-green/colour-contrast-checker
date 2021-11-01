501cd1798be3a1f2ca7b61d3812c653ca314fc48
Tim <github@timgreen.ws>
2021-11-01 14:37:58 +1100

    added const for deleting/removing swatches

--- src/components/molecules/ColorSwatch.jsx
+++ src/components/molecules/ColorSwatch.jsx
@@ -14,3 +14,31 @@ function ColorSwatch() {
     updateContrast(background.rgb, foreground.rgb);
   };
 
+  // Delete saved color
+  const handleDelete = (key) => {
+    const color = savedColors.find((color) => color.time === key);
+    const index = savedColors.indexOf(color);
+
+    // Remove the selected color object
+    const newSavedColors = [...savedColors];
+    newSavedColors.splice(index, 1); // Remove the saved color
+    setSavedColors(newSavedColors);
+    window.localStorage.setItem("colors", JSON.stringify(newSavedColors));
+  };
+
+  return (
+    <Container>
+      {savedColors.map((color) => (
+        <ColorTile
+          key={color.time}
+          background={getRgbString(color.background.rgb)}
+          foreground={getRgbString(color.foreground.rgb)}
+          handleLoad={() => handleLoad(color.time)}
+          handleDelete={() => handleDelete(color.time)}
+        />
+      ))}
+    </Container>
+  );
+}
+
+export default ColorSwatch;
