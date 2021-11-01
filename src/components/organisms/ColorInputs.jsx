97358064ead64d65c5075fdd14787c0ad2661a86
Tim <github@timgreen.ws>
2021-11-01 15:20:52 +1100

    added styles for color inputs

--- src/components/organisms/ColorInputs.jsx
+++ src/components/organisms/ColorInputs.jsx
@@ -4,6 +4,33 @@ import ColorInputField from "../molecules/ColorInputField";
 import InputLabel from "../molecules/InputLabel";
 import { bp } from "../../styles/breakpoints";
 
+const Container = styled.div`
+  width: 100%;
+  max-width: 35rem;
+  padding-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: ${bp.md}) and (max-width: ${bp.lg}) {
+    max-width: 32rem;
+  }
+`;
+
+const ColorInputBlock = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  &:last-child {
+    margin-bottom: .5rem;
+
+    @media (min-width: ${bp.md}) and (max-width: ${bp.lg}) {
+      margin-bottom: 0;
+    }
+  }
+`;
+
 export default function ColorInputs() {
   return (
     <Container>
