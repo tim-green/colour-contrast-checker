9c406985455ef0e5deaf04aa4ac0ffa119bd1704
Tim <github@timgreen.ws>
2021-11-01 14:37:01 +1100

    added function for colour input field

--- src/components/molecules/ColorInputField.jsx
+++ src/components/molecules/ColorInputField.jsx
@@ -1,1 +1,20 @@
 import styled from "styled-components";
+function ColorInputField({ target }) {
+  return (
+    <Container>
+      <ColorTextInput target={target} />
+      <ColorPicker target={target} />
+      <div id={`picker-root-${target}`} />
+      <ButtonGroup>
+        <FormatButton target={target} />
+        <CopyButton target={target} />
+      </ButtonGroup>
+    </Container>
+  );
+}
+
+ColorInputField.propTypes = {
+  target: PropTypes.oneOf(["background", "foreground"]),
+};
+
+export default ColorInputField;
