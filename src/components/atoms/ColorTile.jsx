4cef8f1592f69f52884c28a391eab5d3d857318a
Tim <github@timgreen.ws>
2021-11-01 14:39:21 +1100

    added function for color tile

--- src/components/atoms/ColorTile.jsx
+++ src/components/atoms/ColorTile.jsx
@@ -1,1 +1,28 @@
 import styled from "styled-components";
+
+function ColorTile({ background, foreground, handleLoad, handleDelete }) {
+  return (
+    <Container>
+      <TileBtn
+        bg={background}
+        fg={foreground}
+        onClick={handleLoad}
+        aria-label={`Background ${background} with foreground ${foreground}`}
+      >
+        Aa
+      </TileBtn>
+      <DeleteBtn onClick={handleDelete} aria-label="Delete saved color">
+        <MdClose size={12} />
+      </DeleteBtn>
+    </Container>
+  );
+}
+
+ColorTile.propTypes = {
+  background: PropTypes.string.isRequired,
+  foreground: PropTypes.string.isRequired,
+  handleLoad: PropTypes.func,
+  handleDelete: PropTypes.func,
+};
+
+export default ColorTile;
