492bbc141d7c712f9e9ff656312c9f6e0d31c58b
Tim <github@timgreen.ws>
2021-11-01 12:42:18 +1100

    added theme and utils

--- src/context/ColorContext.jsx
+++ src/context/ColorContext.jsx
@@ -1,3 +1,6 @@
 import { createContext, useState } from "react";
 import { colord } from "colord";
 import PropTypes from "prop-types";
+import { defaultColors } from "../styles/theme";
+import { getContrast } from "../utils/colorUtils";
+
