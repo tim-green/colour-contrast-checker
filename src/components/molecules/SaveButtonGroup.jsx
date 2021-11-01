95833691f4e7ec30fa10e9873622fd53690ff71d
Tim <github@timgreen.ws>
2021-11-01 15:18:42 +1100

    added styles for button group - save

--- src/components/molecules/SaveButtonGroup.jsx
+++ src/components/molecules/SaveButtonGroup.jsx
@@ -2,6 +2,13 @@ import styled from "styled-components";
 import DeleteAllButton from "../atoms/DeleteAllButton";
 import SaveButton from "../atoms/SaveButton";
 
+const ButtonGroup = styled.div`
+  margin: 2.5rem 0 3.5rem;
+  display: flex;
+  gap: 1.5rem 2.5rem;
+  flex-wrap: wrap;
+`;
+
 function SaveButtonGroup() {
   return (
     <ButtonGroup>
