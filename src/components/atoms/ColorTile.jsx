feb28056552587798010351788a15a5d15d3bf87
Tim <github@timgreen.ws>
2021-11-01 15:04:36 +1100

    added styles for tile button, included focus and hover states

--- src/components/atoms/ColorTile.jsx
+++ src/components/atoms/ColorTile.jsx
@@ -7,6 +7,57 @@ const Container = styled.div`
   width: max-content;
 `;
 
+const TileBtn = styled.button`
+  width: 5.55rem;
+  height: 5.55rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid ${(props) => props.fg};
+  border-radius: 1rem;
+  background-color: ${(props) => props.bg};
+  color: ${(props) => props.fg};
+  font-size: 2rem;
+  font-weight: bold;
+  transition: ease-out outline-color .5s;
+
+  &:focus {
+    outline: 3px solid var(--deep-black);
+  }
+
+  &:hover {
+    outline-color: var(--greeny);
+    transition: ease-in-out outline-color .5s;
+    background: rgba(0,0,0,.6);
+    cursor: default;
+  }
+`;
+
+const DeleteBtn = styled.button`
+  width: 2rem;
+  height: 2rem;
+  padding: 0;
+  position: absolute;
+  top: -1rem;
+  right: -1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--red);
+  border: 1px solid var(--light-red);
+  border-radius: 1rem;
+  color: var(--white);
+
+  &:focus {
+    outline: 3px solid var(--deep-black);
+  }
+
+  &:hover {
+    background: var(--light-red);
+    border-color: var(--red);
+  }
+`;
+
 function ColorTile({ background, foreground, handleLoad, handleDelete }) {
   return (
     <Container>
