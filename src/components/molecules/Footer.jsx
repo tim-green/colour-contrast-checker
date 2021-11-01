7aaa61caf758f44eceb4ca50bc1033a6d338f004
Tim <github@timgreen.ws>
2021-11-01 15:28:53 +1100

    added styles for footer container

--- src/components/molecules/Footer.jsx
+++ src/components/molecules/Footer.jsx
@@ -1,2 +1,10 @@
 import styled from "styled-components";
 import { FaGithub } from "react-icons/fa";
+
+const FooterContainer = styled.footer`
+  margin-top: auto;
+  display: flex;
+  align-items: center;
+  color: var(--white);
+`;
+
