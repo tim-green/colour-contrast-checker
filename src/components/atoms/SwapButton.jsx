9d284c0814098f78814075e9130efa0e33f59141
Tim <github@timgreen.ws>
2021-11-01 15:22:09 +1100

    added styles for swap (colour) button

--- src/components/atoms/SwapButton.jsx
+++ src/components/atoms/SwapButton.jsx
@@ -3,6 +3,37 @@ import styled from "styled-components";
 import { MdSwapVert } from "react-icons/md";
 import ColorContext from "../../context/ColorContext";
 import Tooltip from "./Tooltip";
+
+const Button = styled.button`
+  width: 4rem;
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--white-hover);
+  border: none; // Use box-shadow for border instead
+  box-shadow: 0 0 0 2px var(--deep-black);
+  border-radius: 2rem;
+  color: var(--white);
+  font-size: 0rem; // Bug fix: Arrow appear to small on mobile devices
+  transition: 150ms ease;
+  -webkit-appearance: none; // Fix iOS appearance bugs
+
+  &:hover {
+    background-color: transparent;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px var(--white);
+  }
+`;
+
+const tooltipWrapperStyles = {
+  margin: "0 16% 2.2rem 0",
+  padding: "4% 0"
+};
+
 export default function SwapButton() {
   const { handleSwapColors } = useContext(ColorContext);
 
