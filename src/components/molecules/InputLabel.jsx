b00aaffcc07f0fb19c5e1cdebd912792ea235d4a
Tim <github@timgreen.ws>
2021-11-01 15:23:30 +1100

    added styles for input labels

--- src/components/molecules/InputLabel.jsx
+++ src/components/molecules/InputLabel.jsx
@@ -2,6 +2,35 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import { MdInfoOutline } from "react-icons/md";
 import Tooltip from "../atoms/Tooltip";
+
+const Container = styled.div`
+  margin-bottom: 0.8rem;
+  display: flex;
+  align-items: center;
+  color: var(--gray);
+`;
+
+const Label = styled.label`
+  margin-right: 1.2rem;
+  font-size: 1.8rem;
+  font-weight: 600;
+`;
+
+const InfoIcon = styled(MdInfoOutline)`
+  transition: .15s color ease;
+
+  &:hover,
+  &:focus {
+    transition: .15s color ease;
+    color: var(--greeny);
+    outline: none;
+  }
+`;
+
+const tooltipWrapperStyles = {
+  height: "1.8rem",
+};
+
 function InputLabel({ labelFor, title, tooltipText }) {
   return (
     <Container>
