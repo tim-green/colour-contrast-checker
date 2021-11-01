1c35de4c0c45b3f59c244912c29ba6e169a57813
Tim <github@timgreen.ws>
2021-11-01 14:46:27 +1100

    added function for previewtext "block"

--- src/components/molecules/PreviewText.jsx
+++ src/components/molecules/PreviewText.jsx
@@ -1,1 +1,17 @@
 import styled from "styled-components";
+function PreviewText({ children, title, largeText = false }) {
+  return (
+    <Container isLargeText={largeText}>
+      <Title>{title}</Title>
+      <Text>{children}</Text>
+    </Container>
+  );
+}
+
+PreviewText.propTypes = {
+  children: PropTypes.string,
+  title: PropTypes.string,
+  largeText: PropTypes.bool,
+};
+
+export default PreviewText;
