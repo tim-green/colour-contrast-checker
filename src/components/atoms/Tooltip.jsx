4fdcdff23ae66cb70105859d5055df015505e503
Tim <github@timgreen.ws>
2021-11-01 15:17:03 +1100

    added styles for tooltip

--- src/components/atoms/Tooltip.jsx
+++ src/components/atoms/Tooltip.jsx
@@ -1,6 +1,51 @@
 import styled from "styled-components";
 import Tippy from "@tippyjs/react/headless";
 import PropTypes from "prop-types";
+
+const TooltipBox = styled.div`
+  padding: .5rem .9rem;
+  background-color: var(--darkest-gray);
+  border-radius: .5rem;
+  border-color: var(--black);
+
+  /* Style the location of the tooltip arrow */
+  &[data-placement^="top"] > #arrow {
+    bottom: -.4rem;
+  }
+
+  &[data-placement^="bottom"] > #arrow {
+    top: -.4rem;
+  }
+
+  &[data-placement^="left"] > #arrow {
+    right: -.4rem;
+  }
+
+  &[data-placement^="right"] > #arrow {
+    left: -.4rem;
+  }
+`;
+
+const Arrow = styled.div`
+  &,
+  &::before {
+    position: absolute;
+    width: .8rem;
+    height: .8rem;
+    background: inherit;
+  }
+
+  & {
+    visibility: hidden;
+  }
+
+  &::before {
+    visibility: visible;
+    content: "";
+    transform: rotate(45deg);
+  }
+`;
+
 // `children` component needs to wrapped by a span - https://github.com/atomiks/tippyjs-react#component-children
 const ChildrenWrapper = styled.span`
   display: inline-block;
