fbe3b7105f3f097f69f954fd0e9bec383246a025
Tim <github@timgreen.ws>
2021-11-01 12:42:43 +1100

    added color context

--- src/context/ColorContext.jsx
+++ src/context/ColorContext.jsx
@@ -4,3 +4,95 @@ import PropTypes from "prop-types";
 import { defaultColors } from "../styles/theme";
 import { getContrast } from "../utils/colorUtils";
 
+const ColorContext = createContext();
+
+function ColorContextProvider({ children }) {
+  const [background, setBackground] = useState({
+    rgb: colord(defaultColors.background).toRgb(), // ex. { r: 19, g: 42, b: 49, a: 1 }
+    input: defaultColors.background, // ex. "#1d1d1d"
+    inputFormat: "hex",
+    validInput: true,
+  });
+  const [foreground, setForeground] = useState({
+    rgb: colord(defaultColors.foreground).toRgb(),
+    input: defaultColors.foreground,
+    inputFormat: "hex",
+    validInput: true,
+  });
+
+  const [contrast, setContrast] = useState(getContrast(background.rgb, foreground.rgb));
+
+  const updateBackground = (colorState, calcContrast = true) => {
+    const hex = colord(colorState.rgb).toHex();
+    setBackground(colorState);
+    document.documentElement.style.setProperty("--background", hex);
+    calcContrast && updateContrast(colorState.rgb, foreground.rgb);
+  };
+
+  const updateForeground = (colorState, calcContrast = true) => {
+    const hex = colord(colorState.rgb).toHex();
+    setForeground(colorState);
+    document.documentElement.style.setProperty("--foreground", hex);
+    calcContrast && updateContrast(background.rgb, colorState.rgb);
+  };
+
+  const updateContrast = (backgroundRgb, foregroundRgb) => {
+    let contrast = getContrast(backgroundRgb, foregroundRgb);
+    setContrast(contrast);
+  };
+
+  const handleSwapColors = () => {
+    const oldBackground = background;
+    const oldForeground = foreground;
+    updateBackground(foreground);
+    updateForeground(oldBackground);
+    updateContrast(oldBackground.rgb, oldForeground.rgb);
+  };
+
+  // Cycle through color formats of HEX, RGB and HSL
+  const handleChangeFormat = (target) => {
+    const isBackground = target === "background";
+
+    const color = isBackground ? background : foreground;
+    let colorInput = color.input;
+    let colorFormat = color.inputFormat;
+
+    // eslint-disable-next-line 
+    switch (colorFormat) {
+      case "hex":
+        colorInput = colord(color.rgb).toRgbString();
+        colorFormat = "rgb";
+        break;
+      case "rgb":
+        colorInput = colord(color.rgb).toHslString();
+        colorFormat = "hsl";
+        break;
+      case "hsl":
+        colorInput = colord(color.rgb).toHex().toUpperCase();
+        colorFormat = "hex";
+        break;
+    }
+
+    const newColor = { ...color, input: colorInput, inputFormat: colorFormat, validInput: true };
+    isBackground ? updateBackground(newColor, false) : updateForeground(newColor, false);
+  };
+
+  const data = {
+    background,
+    updateBackground,
+    foreground,
+    updateForeground,
+    contrast,
+    updateContrast,
+    handleSwapColors,
+    handleChangeFormat,
+  };
+
+  return <ColorContext.Provider value={data}>{children}</ColorContext.Provider>;
+}
+
+ColorContextProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export { ColorContext as default, ColorContextProvider };
