8e337c8c63e09fd460657be193d9d9788b1a03e6
Tim <github@timgreen.ws>
2021-11-01 14:37:33 +1100

    added function for color swatch

--- src/components/molecules/ColorSwatch.jsx
+++ src/components/molecules/ColorSwatch.jsx
@@ -1,2 +1,16 @@
 import { useContext } from "react";
 import styled from "styled-components";
+function ColorSwatch() {
+  const { updateBackground, updateForeground, updateContrast } = useContext(ColorContext);
+  const { savedColors, setSavedColors } = useContext(SaveContext);
+
+  // Load saved color. Every saved color stores a property called `key`, which is a timestamp
+  const handleLoad = (key) => {
+    const { background, foreground } = savedColors.find((color) => color.time === key);
+
+    // Do not update contrast in `updateBackground` and `updateForeground` since state update is async.
+    updateBackground(background, false);
+    updateForeground(foreground, false);
+    updateContrast(background.rgb, foreground.rgb);
+  };
+
