05365f7804163d0198f4206de0a665f447331087
Tim <github@timgreen.ws>
2021-11-01 15:21:52 +1100

    added styles for outline button

--- src/components/atoms/OutlineButton.jsx
+++ src/components/atoms/OutlineButton.jsx
@@ -1,5 +1,31 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
+
+const Button = styled.button`
+  width: max-content;
+  padding: ${(props) => (props.hasIcon ? "0.7rem 1.4rem 0.7rem 1.2rem" : "0.7rem 1.2rem")};
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  background: none;
+  border: 1px solid ${(props) => props.foreground};
+  border-radius: 0.5rem;
+  color: ${(props) => props.foreground};
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 1.6rem;
+  transition: background-color .15s ease;
+
+  &:hover {
+    background-color: ${(props) => props.hoverColor};
+  }
+
+  &:focus {
+    outline: 2px solid var(--deep-black);
+    background-color: ${(props) => props.hoverColor};
+  }
+`;
+
 function OutlineButton({ anchor, children, color, hoverColor, hrefLink, icon, onClickEvent }) {
   // Additional props if we cast the button into an anchor tag
   const anchorProps = anchor
