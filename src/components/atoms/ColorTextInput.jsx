1351609edfb32e03910ee67aa91662fd4776e71f
Tim <github@timgreen.ws>
2021-11-01 14:39:05 +1100

    added function for color text input

--- src/components/atoms/ColorTextInput.jsx
+++ src/components/atoms/ColorTextInput.jsx
@@ -1,2 +1,47 @@
 import { useContext } from "react";
 import styled from "styled-components";
+function ColorTextInput({ target }) {
+  const { background, updateBackground, foreground, updateForeground } = useContext(ColorContext);
+
+  const isBackground = target === "background";
+  const color = isBackground ? background : foreground;
+  let validInput = color.validInput;
+
+  const handleInputChange = (event) => {
+    const input = event.target.value;
+    const inputColor = colord(input);
+
+    let rgb = inputColor.toRgb();
+    let inputFormat = getFormat(input);
+    validInput = inputColor.isValid();
+
+    // Use values from the original color state if input is invalid
+    if (!validInput) {
+      rgb = color.rgb;
+      inputFormat = color.inputFormat;
+    }
+
+    const newState = { rgb, input, inputFormat, validInput };
+    isBackground ? updateBackground(newState) : updateForeground(newState);
+  };
+
+  return (
+    <>
+      <Input
+        type="text"
+        id={target}
+        value={color.input}
+        onChange={(event) => handleInputChange(event)}
+        valid={validInput}
+        spellCheck={false}
+      />
+      <HelperText visible={!validInput}>Invalid input</HelperText>
+    </>
+  );
+}
+
+ColorTextInput.propTypes = {
+  target: PropTypes.oneOf(["background", "foreground"]).isRequired,
+};
+
+export default ColorTextInput;
